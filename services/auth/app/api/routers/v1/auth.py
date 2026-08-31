4e315620e93e96a54121ddd5bf97eb6b19bf943d
saveliyu <uljanoff.sav@yandex.ru>
2026-08-31 13:47:59 +0700

    auth router stubs

--- services/auth/app/api/routers/v1/auth.py
+++ services/auth/app/api/routers/v1/auth.py
@@ -1,7 +1,36 @@
-from fastapi import APIRouter
+from datetime import datetime
+from typing import Annotated
+
+from fastapi import APIRouter, Body, status
+
+from app.api.schemas.tokens import TokenResponse, TokenRequest
+from app.api.schemas.types import UserRole
+from app.api.schemas.user import UserRegister, UserRead, UserLogin
 
 router = APIRouter(prefix="/auth")
+
 
 @router.get("/health-check")
 async def health_check():
     return {"status": "ok"}
+
+
+@router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def register(data: Annotated[UserRegister, Body()]):
+    return UserRead(id=1, full_name=data.full_name, phone_number=data.phone_number, role=UserRole.CUSTOMER,
+                    created_at=datetime.now())
+
+
+@router.post("/login", response_model=TokenResponse)
+async def login(data: Annotated[UserLogin, Body()]):
+    return TokenResponse(access_token="aaa", refresh_token="aaa")
+
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+async def logout(data: Annotated[TokenRequest, Body()]):
+    return
+
+
+@router.post("/refresh", response_model=TokenResponse)
+async def refresh(data: Annotated[TokenRequest, Body()]):
+    return TokenResponse(access_token="aaa", refresh_token="aaa")
